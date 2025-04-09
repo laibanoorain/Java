ba443701e4748e916d6b69a2a5524114c12aca04
laibanoorain <laibanoorain4@gmail.com>
2025-04-09 08:49:07 +0530

    saved

---
+++ java/Add.java
@@ -0,0 +1,16 @@
+package java.java;
+
+public class Add {
+
+        public static void main(String[] args){
+            {
+                int a=10;
+                int b=5;
+                int sum=a+b;
+                System.out.println("sum is:"+sum);
+            }
+            
+        }
+    }
+
+
